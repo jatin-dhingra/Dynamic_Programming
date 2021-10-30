f91adc22a81815ce08e6b85cee6ef5eac2ccc423
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2021-10-30 14:44:49 +0530

    Create 01_kanpsack.cpp
    Simple Code of 01 KnapSack

---
+++ 01_kanpsack.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int knapSack(int val[],int wt[],int W,int n)
+{
+	if (n == 0 || W == 0)
+		return 0;
+
+  if(wt[n-1]<=W)
+		return max(val[n-1]+knapSack(val,wt,W-wt[n-1],n-1),knapSack(val,wt,W,n-1));
+
+
+	else if (wt[n - 1] > W)
+		return knapSack(val,wt,W,n-1);
+}
+
+int main()
+{
+	int val[] = { 60, 100, 120 };
+	int wt[] = { 10, 20, 30 };
+	int W = 50;
+	int n=3;
+	cout << knapSack(val,wt,W,n);
+
+}
