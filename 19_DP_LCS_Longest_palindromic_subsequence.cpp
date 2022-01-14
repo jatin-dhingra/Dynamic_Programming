881acd4587b6bdd11b42d2ae243f815df14323f6
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-01-15 03:37:45 +0530

    Update 19_DP_LCS_Longest_palindromic_subsequence.cpp

--- 19_DP_LCS_Longest_palindromic_subsequence.cpp
+++ 19_DP_LCS_Longest_palindromic_subsequence.cpp
@@ -1,26 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int static dp[1001][1001];
-int lcs(string x,string y,int m,int n)
+int LPS(string s,string k,int m,int n)
 {
-
-  for(int i{};i<m+1;i++){
-    for(int j{};j<n+1;j++){
-      if(i==0 || j==0){
+  int dp[m+1][n+1];
+  for(int i{};i<=m;i++)
+  {
+    for(int j{};j<=n;j++)
+    {
+      if(i==0||j==0)
+      {
         dp[i][j]=0;
       }
     }
   }
-  for(int i{1};i<m+1;i++)
+  for(int i{1};i<=m;i++)
   {
-    for(int j{1};j<n+1;j++){
-      if(x[i-1]==y[j-1])
+    for(int j{1};j<=n;j++)
+    {
+      if(s[i-1]==k[j-1])
       {
-        dp[i][j]=1+dp[i-1][j-1];
+        dp[i][j]=dp[i-1][j-1]+1;
       }
       else
       {
-        dp[i][j]=0;
+        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
       }
     }
   }
@@ -28,16 +31,12 @@ int lcs(string x,string y,int m,int n)
 }
 int main()
 {
-  string x{};
-  cin>>x;
-  string y;
-  int m=x.size();
-  for(int i=m;i>=0;i--)
-  {
-      y.push_back(s[i]);
-  }
-
-  int n=y.size();
-
-  cout<<lcs(x,y,m,n);
+  string s;
+  cin>>s;
+  string t=s;
+  reverse(t.begin(),t.end());
+  int n=s.size();
+  int m=t.size();
+  cout<<"LPS"<<LPS(s,t,n,m)<<endl;
+  cout<<"min number of deletion: "<<n-LPS(s,t,n,m)<<endl;
 }
