a9b413ba9d591b06f97901ed313ef0796302fa13
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-01-23 17:58:13 +0530

    Create 28_DP_MCM_(36)_palindrom_partition.cpp

---
+++ 28_DP_MCM_(36)_palindrom_partition.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isplaindrome(string s,int i,int j)
+{
+  int len=j-i+1;
+  for(int k=i;k<len/2;k++)
+  {
+    if(s[k]!=s[len-i-1])
+    {
+      return false;
+    }
+  }
+  return true;
+
+}
+int solve(string s,int i,int j)
+{
+  if(i>=j){
+    return 0;
+  }
+  if(isplaindrome(s,i,j))
+  {
+    return 0;
+  }
+  int ans=INT_MAX;
+  for(int k=i;k<=j-1;k++)
+  {
+    int temp=solve(s,i,k)+solve(s,k+1,j)+1;
+    ans=min(ans,temp);
+  }
+  return ans;
+
+}
+
+
+int main()
+{
+  string s;
+  cin>>s;
+  int i=0;
+  int j=s.size()-1;
+  cout<<solve(s,i,j);
+
+}
